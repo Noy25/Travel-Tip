a679f80079b392e5816dd2ac2387d9e330144f78
shahaf <shahaf1131@gmail.com>
2021-12-22 15:19:20 +0200

    first prefix

--- js/services/location.service.js
+++ js/services/location.service.js
@@ -3,14 +3,12 @@ export const locService = {
 }
 
 const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
+    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
     { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
 ]
 
 function getLocs() {
     return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(locs);
-        }, 2000)
-    });
+        resolve(locs);
+    })
 }
